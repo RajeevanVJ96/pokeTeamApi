66c12b00fb2a54ae5b45443e4a793e25788fe1a2
RajeevanVJ96 <rvijay46@gmail.com>
2019-09-09 18:33:10 +0100

    can connect to api

--- src/main/java/com/qa/controllers/PokeController.java
+++ src/main/java/com/qa/controllers/PokeController.java
@@ -3,6 +3,7 @@ package com.qa.controllers;
 import com.qa.models.Pokemon;
 import com.qa.repository.PokeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,23 +15,30 @@ public class PokeController {
     @Autowired
     private PokeRepository repository;
 
-    @RequestMapping(value = "pokemons", method = RequestMethod.GET)
+    @RequestMapping(value = "pokemon", method = RequestMethod.GET)
     public List<Pokemon> listAllPokemon(){
         return repository.findAll();
     }
 
-    @RequestMapping(value = "pokemons", method = RequestMethod.POST)
+    @RequestMapping(value = "pokemon", method = RequestMethod.POST)
     public Pokemon addPokemon(@RequestBody Pokemon poke){
         return repository.saveAndFlush(poke);
     }
 
-    @RequestMapping(value = "pokemons/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "pokemon/{id}", method = RequestMethod.GET)
     public Pokemon getPokemon(@PathVariable Long id){
         return repository.findOne(id);
 
     }
 
-    @RequestMapping(value = "pokemons/{id}", method = RequestMethod.DELETE)
+    @Transactional
+    @RequestMapping(value = "pokemon/{id}", method = RequestMethod.PUT)
+    public void updatePokemon(@PathVariable Long id, @RequestBody Pokemon poke){
+        Pokemon p = repository.findOne(id);
+        p.setPokemon(poke);
+    }
+
+    @RequestMapping(value = "pokemon/{id}", method = RequestMethod.DELETE)
     public void delPokemon(@PathVariable Long id){
         repository.delete(id);
     }
--- src/main/java/com/qa/models/Pokemon.java
+++ src/main/java/com/qa/models/Pokemon.java
@@ -11,16 +11,26 @@ public class Pokemon {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     Long id;
-    private String name;
-    private int level;
-    private String type;
-    private String nature;
-    private String m1;
-    private String m2;
-    private String m3;
-    private String m4;
-    private String description;
+    String name;
+    int pid;
+    String m1;
+    String m2;
+    String m3;
+    String m4;
 
+    public Pokemon (){
+        super();
+    }
+
+    public Pokemon(String name, int pid, String m1, String m2, String m3, String m4) {
+        super();
+        this.name = name;
+        this.pid = pid;
+        this.m1 = m1;
+        this.m2 = m2;
+        this.m3 = m3;
+        this.m4 = m4;
+    }
 
     public Long getId() {
         return id;
@@ -34,32 +44,16 @@ public class Pokemon {
         return name;
     }
 
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getNature() {
-        return nature;
-    }
-
-    public void setNature(String nature) {
-        this.nature = nature;
     }
 
     public String getM1() {
@@ -94,12 +88,13 @@ public class Pokemon {
         this.m4 = m4;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    public void setPokemon(Pokemon p){
+        this.name = p.name;
+        this.pid = p.pid;
+        this.m1 = p.m1;
+        this.m2 = p.m2;
+        this.m3 = p.m3;
+        this.m4 = p.m4;
     }
 
 }
